3b355a381542fd99ad77504e144afef650e15c59
vdweegen <cas@vdweegen.com>
2017-12-09 16:01:37 +0100

    Have PieChart (PyChart) draw based on the percentage of SLOC

--- Series2Visualizer/pychart/draw.py
+++ Series2Visualizer/pychart/draw.py
@@ -1,26 +1,47 @@
 from tkinter import *
 from random import randint
 
+
 # these values define the coordinate system for the returned rectangles
 # the values will range from x to x + width and y to y + height
 class PyChart:
-    def prop(self, n):
-        return 360.0 * n / 1000
+    width = 500
+    height = 500
+    project_sloc = 0
 
-    def vals(self, values):
-        # TODO: values -> vals
-        self.vals = {}
-        # Currently draws random pycharts
-        self.vals['q1'] = randint(200,250) # Type 1 Clones
-        self.vals['q2'] = randint(200,400) # Type 2 Clones
-        self.vals['q3'] = randint(200,350) # Type 3 Clones
-        self.vals['q4'] = 1000 - (self.vals['q1'] + self.vals['q2'] + self.vals['q3']) # Total No of Lines
+    def prop(self, n):
+        return 360.0 * n / self.project_sloc
+
+    def vals(self, project):
+        self.project_sloc = project.get_sloc()
+
+        self.vals = {'q1': 0, 'q2': 0, 'q3': 0, 'q4': 0}
+
+        # Classes with clones
+        for cls in project.CLASSES:
+            for cos in cls:
+                if cos.get_type() == 1:  # Type 1 Clones
+                    self.vals['q1'] += cos.get_sloc()
+                if cos.get_type() == 2:  # Type 2 Clones
+                    self.vals['q2'] += cos.get_sloc()
+                if cos.get_type() == 3:  # Type 3 Clones
+                    self.vals['q3'] += cos.get_sloc()
+
+        self.vals['q4'] = self.project_sloc - (self.vals['q1'] + self.vals['q2'] + self.vals['q3'])  # Total No of Lines
 
     def draw(self, canv):
-        c = Canvas(canv, width=154, height=154)
+        c = Canvas(canv, width=self.width, height=self.height)
         c.pack()
-        c.create_arc((2,2,152,152), fill="#FAF402", outline="#FAF402", start=self.prop(0), extent = self.prop(self.vals['q1']))
-        c.create_arc((2,2,152,152), fill="#00AC36", outline="#00AC36", start=self.prop(self.vals['q1']), extent = self.prop(self.vals['q2']))
-        c.create_arc((2,2,152,152), fill="#7A0871", outline="#7A0871", start=self.prop(self.vals['q1']+self.vals['q2']), extent = self.prop(self.vals['q3']))
-        c.create_arc((2,2,152,152), fill="#E00022", outline="#E00022", start=self.prop(self.vals['q1']+self.vals['q2']+self.vals['q3']), extent = self.prop(self.vals['q4']))
+        c.create_arc((2, 2, self.width, self.height), fill="#FAF402", outline="#FAF402",
+                     start=self.prop(0),
+                     extent=self.prop(self.vals['q1']))
+        c.create_arc((2, 2, self.width, self.height), fill="#00AC36", outline="#00AC36",
+                     start=self.prop(self.vals['q1']),
+                     extent=self.prop(self.vals['q2']))
+        c.create_arc((2, 2, self.width, self.height), fill="#7A0871", outline="#7A0871",
+                     start=self.prop(self.vals['q1'] + self.vals['q2']),
+                     extent=self.prop(self.vals['q3']))
+        c.create_arc((2, 2, self.width, self.height), fill="#E00022", outline="#E00022",
+                     start=self.prop(self.vals['q1'] + self.vals['q2'] + self.vals['q3']),
+                     extent=self.prop(self.vals['q4']))
         canv.update()
